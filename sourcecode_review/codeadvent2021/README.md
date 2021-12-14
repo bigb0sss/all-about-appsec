dd520631812d27a628a81bd55d5a43454d4f4446
bigb0ss <49355913+bigb0sss@users.noreply.github.com>
2021-12-13 23:44:35 -0500

    challenge-02

--- sourcecode_review/codeadvent2021/README.md
+++ sourcecode_review/codeadvent2021/README.md
@@ -1,4 +1,5 @@
 # SonarSource - codeadvent2021
 
 2021 daily sourcecode challenges
-- Challenge-01 (JavaScript)
+- [Challenge-01](https://github.com/bigb0sss/all-about-appsec/blob/main/sourcecode_review/codeadvent2021/challenge_01.html) (JavaScript) - DOM XSS
+- [Challenge-02](https://github.com/bigb0sss/all-about-appsec/blob/main/sourcecode_review/codeadvent2021/challenge_02.py) (Python) - Arbitrary File Read (os.join.path())
---
+++ sourcecode_review/codeadvent2021/challenge_02.py
@@ -0,0 +1,29 @@
+import os
+
+from django.http import HttpResponse
+
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+def get_image(request):
+    filename = request.POST['img']
+    file_path = os.path.join(BASE_DIR, "uploads", filename)
+    if file_path.find(".") != -1:
+        return HttpResponse("Failed!")
+    else:
+        with open(file_path) as f:
+            return HttpResponse(f.read(), content_type='image/png')
+
+"""
+# Issues
+Arbitrary file read - os.path.join() can take an absolute file path (e.g., /etc/passwd) from the 'img' parameter as the filename. The official Python document (https://docs.python.org/3/library/os.path.html?highlight=thrown%20away#os.path.join) stated that if os.path.join() sees an absolute file path, it will ignore any previous values.
+
+For example, 
+```
+>>> import os
+>>> os.path.join('/bigb0ss', '/etc/passwd')
+'/etc/passwd'
+```
+
+# Mitigation
+- Sanitize the user supplied filename not to contain special characters like "/" 
+"""
